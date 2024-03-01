0addaba3479fb8580f72ec262923a3fccbb1abf2
Sara Shaikh <sarashaikh@Saras-MacBook-Air.local>
2024-03-01 16:19:23 +0530

    FIRST COMMIT

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,21 @@
+const Navigation=()=>{
+    return(
+        <nav className="container">
+
+      <div className="logo">
+        <img src="/images/logo.png" />
+      </div>
+
+      <ul>
+        <li href="#"> Menu</li>
+        <li href="#"> Location</li>
+        <li href="#"> Menu</li>
+        <li href="#"> Menu</li>
+      </ul>
+
+
+      <button> Login</button>
+    </nav>
+    )
+};
+export default Navigation;
